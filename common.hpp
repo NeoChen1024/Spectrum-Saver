80febd28d63d698d699ffbd78818044d7835797f
Neo_Chen <chenkolei@gmail.com>
2023-03-17 11:35:47 +0800

    common.hpp: added test_error() for error messages
    Former-commit-id: 0896bb7cc6d357fd4fa7b2586df61ec6dd36cf48

--- common.hpp
+++ common.hpp
@@ -2,12 +2,23 @@
 #include <string>
 
 using std::string;
+using std::cerr;
+using std::endl;
 
-const string time_str(void)
+static const string time_str(void)
 {
 	char time_str[256];
 	time_t current_time = time(NULL);
 	strftime(time_str, sizeof(time_str), "%Y%m%dT%H%M%S", gmtime(&current_time));
 
 	return string(time_str);
+}
+
+void static test_error(bool condition, const string &message)
+{
+	if(condition)
+	{
+		cerr << message << endl;
+		exit(1);
+	}
 }
