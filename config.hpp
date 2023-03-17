902458b5869eff309f27b589dd3dae0e50aceed2
Neo_Chen <chenkolei@gmail.com>
2023-03-18 01:19:41 +0800

    config: include both zero level for tinySA & tinySA Ultra
    Former-commit-id: 78cd3e53b15fe572882aecc58f3a62a854999a6a

--- config.hpp
+++ config.hpp
@@ -3,11 +3,14 @@
 /* options used by spsave: */
 
 // tinySA Zero Level, 128 for tinySA, 174 for tinySA Ultra
-#define ZERO_LEVEL 174
+const static int ZERO_LEVEL = 128;
+const static int  ZERO_LEVEL_ULTRA = 174;
 
 /* options used by log2png: */
 
 // Font for banner text
 #define FONT_FAMILY	"Liberation Mono"
+
+// Also used by spsave to determine when to rotate log file
 // 1440 records would be 24 hours at 1 minute interval
 #define MAX_RECORDS	1440 
