cbdbfecc1e5534344e241415d15b29f8cb4de0c2
Neo_Chen <chenkolei@gmail.com>
2023-03-18 13:54:56 +0800

    header-only fmt

--- common.hpp
+++ common.hpp
@@ -1,9 +1,9 @@
 #include <ctime>
 #include <string>
 
+#define FMT_HEADER_ONLY
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <fmt/format-inl.h>
 #include <fmt/chrono.h>
 #include <fmt/color.h>
 #include <fmt/ostream.h>
